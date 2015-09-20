69c7736fd02bbfcce39e425d0a559aedc07a340e
梁杰 <lj925184928@gmail.com>
2015-09-20 22:39:26 +0800

    update

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,13 +1,11 @@
 Zepto(function($) {
   function adjustBg() {
-    $(".speaker__content").height($(".speaker__content").width())
-    $(".speaker__card img")
-      .width(Math.round($(".speaker__content").width() * 0.618))
-      .height(Math.round($(".speaker__content").width() * 0.618))
-      .css({
-        margin: Math.round($(".speaker__content").width() * 0.382) / 2,
-        borderRadius: Math.round($(".speaker__content").width() * 0.618) / 2,
 
+    $(".speaker__card img")
+      .width(Math.round($(".speaker__content").width() * 0.5))
+      .height(Math.round($(".speaker__content").width() * 0.5))
+      .css({
+        borderRadius: Math.round($(".speaker__content").width() * 0.25)
       })
     $(".info").height($(window).height())
     var height = $(window).height()
@@ -17,6 +15,14 @@ Zepto(function($) {
     } else {
       $(".info").css("background-size", (height * 1.95) + "px " + (height) + "px")
     }
+
+    var max = 0
+    $(".speaker__content").map(function() {
+      if ($(this).height() > max) {
+        max = $(this).height()
+      }
+    })
+    $(".speaker__content").height(max)
   }
   $(window).resize(adjustBg)
   adjustBg()
